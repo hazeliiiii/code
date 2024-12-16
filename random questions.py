7715c53932e6f7335b7e44f0e82d7e050cd867d2
hazeliiiii <19bmills@keshacademy.com>
2024-12-16 09:24:44 +0000

    Update random questions.py

--- random questions.py
+++ random questions.py
@@ -1,1 +1,47 @@
-{"mode":"python","type":"text","code":"# list of whats needed:\n# 20 diff random numbers\n# 4 random signs per question\n\nfrom random import *\n\nflag = 1\nscore = 0\n\ndef question_ask():\n    while flag == 1:\n        try:\n            answer = int(input(\"What is the answer? \"))\n            return (answer)\n        except ValueError:\n            print (\"Needs to be an integer!\")\n\ndef questions():\n    x = randint(1,10)\n    y = randint(1,10)\n    z = randint(1,3)\n\n    if z == 1:\n        solution = x + y\n        sign = \"+\"\n    elif z == 2:\n        solution = x - y\n        sign = \"-\"\n    elif z == 3:\n        solution = x * y\n        sign = \"x\"\n\n    print(\"What is\", str(x), str(sign), str(y)+\"?\")\n    u_input = question_ask()\n\n    if u_input == solution:\n        print(\"Correct!\")\n        global score\n        score = score + 1\n    else:\n        print(\"Incorrect! The answer was:\", solution)\n\nname = input(\"What is your name? \")\namount = int(input(\"How many questions do you want to do?\" ))\n\nfor i in range(amount):\n    questions()\n\nprint (\"You got\", score, \"/\", amount)","extensions":null}
+# list of whats needed:
+# 20 diff random numbers
+# 4 random signs per question
+
+from random import *
+flag = 1
+score = 0
+
+def question_ask():
+    while flag == 1:
+        try:
+            answer = int(input("What is the answer? "))
+            return (answer)
+        except ValueError:
+            print ("Needs to be an integer!")
+            
+def questions():
+    x = randint(1,10)
+    y = randint(1,10)
+    z = randint(1,3)    
+    
+    if z == 1:
+        solution = x + y
+        sign = "+"
+    elif z == 2:
+        solution = x - y
+        sign = "-"
+    elif z == 3:
+        solution = x * y
+        sign = "x"
+        
+        print("What is", str(x), str(sign), str(y)+"?")
+        u_input = question_ask()
+        
+        if u_input == solution:
+            print("Correct!")
+            global score
+            score = score + 1
+        else:
+            print("Incorrect! The answer was:", solution)
+            
+name = input("What is your name? ")
+amount = int(input("How many questions do you want to do?"  ))
+
+for i in range(amount):
+    questions()
+    print ("You got", score, "/", amount)
